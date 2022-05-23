7d43b9b2847fe021fe46d9c560c348c801c552ca
virgile blua <86666280+tikitong@users.noreply.github.com>
2022-05-23 16:25:49 +0200

    Format.

--- script.py
+++ script.py
@@ -1,8 +1,4 @@
 # Fast script for the creation of a sub-set of the coco dataset in the form of a data folder.
-#   data/
-#     images/ *.jpg
-#     labels/ train.json
-#             val.json
 
 import json
 from pycocotools.coco import COCO
@@ -17,28 +13,20 @@ ANNOTATIONS = {"info": {
 }
 }
 
-
 def myImages(images: list, train: int, val: int) -> tuple:
-
     myImagesTrain = images[:train]
     myImagesVal = images[train:train+val]
-
     return myImagesTrain, myImagesVal
 
 
 def cocoJson(images: list) -> dict:
-
     arrayIds = np.array([k["id"] for k in images])
-
     annIds = coco.getAnnIds(imgIds=arrayIds, catIds=catIds, iscrowd=None)
     anns = coco.loadAnns(annIds)
-
     for k in anns:
         k["category_id"] = catIds.index(k["category_id"])+1
-
     catS = [{'id': int(value), 'name': key}
             for key, value in categories.items()]
-
     ANNOTATIONS["images"] = images
     ANNOTATIONS["annotations"] = anns
     ANNOTATIONS["categories"] = catS
@@ -47,27 +35,21 @@ def cocoJson(images: list) -> dict:
 
 
 def createJson(JsonFile: json, train: bool) -> None:
-
     name = "train"
     if not train:
         name = "val"
-
     Path("data/labels").mkdir(parents=True, exist_ok=True)
-
     with open(f"data/labels/{name}.json", "w") as outfile:
         json.dump(JsonFile, outfile)
 
 
 def downloadImages(img: dict) -> None:
-
     link = (img['coco_url'])
-
     Path("data/images").mkdir(parents=True, exist_ok=True)
-
     wget.download(link, f"{'data/images/' + img['file_name']}")
 
 
-# instantiate COCO specifying the annotations json path; download here: https://cocodataset.org/#download
+# Instantiate COCO specifying the annotations json path; download here: https://cocodataset.org/#download
 coco = COCO('instances_train2017.json')
 
 # Specify a list of category names of interest
@@ -81,9 +63,9 @@ dictCOCOSorted = dict(sorted(dictCOCO.items(), key=lambda x: x[1]))
 IdCategories = list(range(1, len(catNms)+1))
 categories = dict(zip(list(dictCOCOSorted), IdCategories))
 
-''''getCatIds return a sorted list of id. 
-For the creation of the json file in coco format, the list of ids must be successive 1, 2, 3.. 
-so we reorganize the ids. In the cocoJson method we modify the values of the category_id parameter '''
+# getCatIds return a sorted list of id.
+# For the creation of the json file in coco format, the list of ids must be successive 1, 2, 3..
+# So we reorganize the ids. In the cocoJson method we modify the values of the category_id parameter.
 
 # Get the corresponding image ids and images using loadImgs
 imgIds = coco.getImgIds(catIds=catIds)
@@ -101,11 +83,9 @@ createJson(trainSet, train=True)
 valSet = cocoJson(myImagesVal)
 createJson(valSet, train=False)
 
-print("downloading training set...")
 Parallel(
     n_jobs=-1, prefer="threads")([delayed(downloadImages)(img) for img in myImagesTrain])
 
-print("\ndownloading validation set...")
 Parallel(
     n_jobs=-1, prefer="threads")([delayed(downloadImages)(img) for img in myImagesVal])
 
